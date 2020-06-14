9436ecc4796440395582ce0234054d8787f3949b
Tiago Silva <tiago.toya@hotmail.com>
2020-06-14 14:50:18 -0300

    fix: ajuste no path join

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 const fs = require('fs')
 const path = require('path')
 
-const domainsPaths = path.join(process.cwd(), './src/domains')
+const domainsPaths = path.join(process.cwd(), 'src', 'domains')
 
 const domains = fs.readdirSync(domainsPaths)
 
 for (const domain of domains) {
-  require(path.join(process.cwd(), './src/domains', domain))
+  require(path.join(process.cwd(), 'src', 'domains', domain))
 }
